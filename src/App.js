2fdfefc7ffdb94106c2d5820c3b21c1d847e35ed
Arjun Dey <arjun.dey.2612@gmail.com>
2022-12-17 21:23:04 +0530

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [make, setMake] = useState("");
+  const [turn, setTurn] = useState(0);
+  const [iValue, setIValue] = useState("");
+
+  function hoverMovement() {
+    if (turn === 0) {
+      setMake("translateX(150px)");
+      setTurn(turn + 1);
+    } else {
+      setMake("translateX(0px)");
+      setTurn(turn - 1);
+    }
+  }
+
+  var translate = make;
+
+  const iHandler = (event) => {
+    setIValue(event.target.value);
+    console.log(iValue);
+  };
+
+  function submitMove() {
+    if (iValue.length < 8) {
+      return translate;
+    } else {
+      return "none";
+    }
+  }
+
+  function submitColor() {
+    if (iValue.length < 8) {
+      return "#f87171";
+    } else {
+      return "#22c55e";
+    }
+  }
+
+  function submitBorder() {
+    if (iValue.length < 8) {
+      return "2px solid #f87171";
+    } else {
+      return "2.5px solid #22c55e";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="container">
+        <h1>Sign Up</h1>
+        <input className="username" placeholder="Enter Username" type="text" />
+        <input
+          type="password"
+          className="password"
+          placeholder="Enter Password"
+          onChange={iHandler}
+          style={{
+            border: submitBorder(),
+            color: submitColor()
+          }}
+        />
+        <div className="btn-div">
+          <button
+            className="btn"
+            onMouseEnter={() => hoverMovement()}
+            style={{
+              border: submitBorder(),
+              color: submitColor(),
+              transform: submitMove(),
+              cursor: "pointer"
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
